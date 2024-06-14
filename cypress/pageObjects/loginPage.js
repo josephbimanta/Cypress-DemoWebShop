719c7816ed892d68252cd8d7825ea517f3ea65ab
josephbimanta <joseph.bimanta@gmail.com>
2024-06-14 19:09:06 +0700

    Update script

--- cypress/pageObjects/loginPage.js
+++ cypress/pageObjects/loginPage.js
@@ -37,6 +37,16 @@ class loginPage {
       this.elements.getLi();
       cy.contains(message);
     }
+
+    loginWithValidCredentials(){
+      cy.get('.ico-login').click()
+      this.elements.inputEmail().clear();
+      this.elements.inputEmail().type('joseph.qa4@gmail.com');
+      this.elements.inputPassword().clear();
+      this.elements.inputPassword().type('testingqa');
+      cy.get('form > .buttons > .button-1').click();
+      cy.get('.ico-logout').should('contain.text', 'Log out');
+    }
 }
 
 export default new loginPage()
